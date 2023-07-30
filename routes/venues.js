ecc3ad1807a1e40219e0df578a46967076b8d083
staceyjf <staceyfanner@gmail.com>
2023-07-30 15:04:57 +1000

    New venue form add
    [Ticket: 7]

--- routes/venues.js
+++ routes/venues.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const venuesController = require('../controllers/venues')
 const ensureLoggedIn = require('../config/ensureLoggedIn');
 
+/*
+params that can be extracted from the url - if you need to pass params to the function, you can do so using query params or URL
+function to handle the request
+*/
+
 // GET /venues  View all venues regardless of logged in status
 router.get('/', venuesController.index);
 
@@ -11,6 +16,7 @@ router.get('/', venuesController.index);
 
 // GET /venues/new 
 //MUST be below new route /	View a form for submitting a venue (be sure to define this route before the show route)
+// it would mean that the route is designed to handle requests with the "/new" part as a dynamic parameter. In this context, "/new" is not a fixed part of the URL but a placeholder for a variable value that can change with each request.
 router.get('/new', ensureLoggedIn, venuesController.new);
 
 // GET /venues/:id View the details of a venue
